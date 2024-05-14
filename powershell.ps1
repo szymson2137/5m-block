0a1364a75506d882d7e7419f17b6fb32f89c87f3
Szymon <103281923+szymson2137@users.noreply.github.com>
2024-05-15 00:47:52 +0200

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -20,3 +20,18 @@ foreach ($user in $users) {
     New-NetFirewallRule -DisplayName "Blokuj FiveM" -Direction Inbound -Program $path -Action Block
     New-NetFirewallRule -DisplayName "Blokuj FiveM" -Direction Outbound -Program $path -Action Block
 }
+
+# Pobierz istniejące reguły zapory sieciowej
+$existingRules = Get-NetFirewallRule
+
+# Sprawdź, czy reguła dla server.pixarp.com już istnieje
+$ruleExists = $existingRules | Where-Object { $_.DisplayName -eq "Block server.pixarp.com" }
+
+if (-not $ruleExists) {
+    # Dodaj nową regułę blokującą ruch z server.pixarp.com
+    New-NetFirewallRule -DisplayName "Block server.pixarp.com" -Direction Outbound -RemoteAddress "server.pixarp.com" -Action Block
+    Write-Host "Reguła została dodana: Block server.pixarp.com"
+} else {
+    Write-Host "Reguła już istnieje: Block server.pixarp.com"
+}
+
