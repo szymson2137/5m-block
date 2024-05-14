99f592dd5ee5f00d4082194c8b077492c885eef2
Szymon <103281923+szymson2137@users.noreply.github.com>
2024-05-15 01:29:02 +0200

    Update

---
+++ unlock.ps1
@@ -0,0 +1,8 @@
+# Pobierz wszystkie reguły zapory sieciowej
+$allRules = Get-NetFirewallRule
+
+# Wyłącz reguły zawierające "FiveM" w nazwie
+$rulesToDisable = $allRules | Where-Object { $_.DisplayName -like "*FiveM*" }
+$rulesToDisable | ForEach-Object {
+    Set-NetFirewallRule -Name $_.Name -Enabled False
+}
