28cdeca489ee691801904b3047c202062221040e
Szymon <103281923+szymson2137@users.noreply.github.com>
2024-05-15 00:36:49 +0200

    Add files via upload

---
+++ powershell.ps1
@@ -0,0 +1,15 @@
+$allRules = Get-NetFirewallRule
+
+$rulesToDisable = $allRules | Where-Object { $_.DisplayName -like "*FiveM*" }
+$rulesToDisable | ForEach-Object {
+    Set-NetFirewallRule -Name $_.Name -Enabled False
+}
+
+New-NetFirewallRule -DisplayName "Blokuj port 30120" -Direction Inbound -LocalPort 30120 -Protocol TCP -Action Block
+New-NetFirewallRule -DisplayName "Blokuj port 30120" -Direction Outbound -LocalPort 30120 -Protocol TCP -Action Block
+
+New-NetFirewallRule -DisplayName "Blokuj port 30110" -Direction Inbound -LocalPort 30110 -Protocol UDP -Action Block
+New-NetFirewallRule -DisplayName "Blokuj port 30110" -Direction Outbound -LocalPort 30110 -Protocol UDP -Action Block
+
+New-NetFirewallRule -DisplayName "Blokuj port 40120" -Direction Inbound -LocalPort 40120 -Protocol TCP -Action Block
+New-NetFirewallRule -DisplayName "Blokuj port 40120" -Direction Outbound -LocalPort 40120 -Protocol TCP -Action Block
