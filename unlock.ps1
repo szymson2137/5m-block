55bea531033c02198aa727fed54859f0f83f1824
Szymon <103281923+szymson2137@users.noreply.github.com>
2024-05-15 13:10:15 +0200

    Update unlock.ps1

--- unlock.ps1
+++ unlock.ps1
@@ -1,8 +1,17 @@
-# Pobierz wszystkie reguły zapory sieciowej
-$allRules = Get-NetFirewallRule
+# Usuń reguły blokujące porty 30120, 30110 i 40120
+Get-NetFirewallRule -DisplayName "Blokuj port 30120" | Remove-NetFirewallRule
+Get-NetFirewallRule -DisplayName "Blokuj port 30110" | Remove-NetFirewallRule
+Get-NetFirewallRule -DisplayName "Blokuj port 40120" | Remove-NetFirewallRule
 
-# Wyłącz reguły zawierające "FiveM" w nazwie
-$rulesToDisable = $allRules | Where-Object { $_.DisplayName -like "*FiveM*" }
-$rulesToDisable | ForEach-Object {
+# Usuń regułę blokującą FiveM.exe
+Get-NetFirewallRule -DisplayName "Blokuj FiveM" | Remove-NetFirewallRule
+
+# Włącz wszystkie reguły zawierające 'FiveM' w nazwie
+$allRules = Get-NetFirewallRule
+$rulesToEnable = $allRules | Where-Object { $_.DisplayName -like "*FiveM*" }
+$rulesToEnable | ForEach-Object {
     Set-NetFirewallRule -Name $_.Name -Enabled True
+}
+
+Write-Host "Wszystkie reguły zawierające 'FiveM' zostały włączone."
 }
