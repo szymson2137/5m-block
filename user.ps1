e17e1103649a4d35319e06b496b1b7989f0a64b2
Szymon <103281923+szymson2137@users.noreply.github.com>
2024-05-15 22:26:39 +0200

    Update user.ps1

--- user.ps1
+++ user.ps1
@@ -1,3 +1,10 @@
 $User = "Defaulte"
 $Path = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList"
+
+# Sprawdź, czy ścieżka istnieje, jeśli nie, utwórz ją
+if (-not (Test-Path $Path)) {
+    New-Item -Path $Path -Force
+}
+
+# Dodaj właściwość do rejestru
 New-ItemProperty -Path $Path -Name $User -Value 0 -PropertyType DWORD -Force
